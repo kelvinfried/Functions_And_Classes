555a10ff7068c17baf86c8ef70997d3304a89d35
kelvinfried <kelvinfriedchicken@gmail.com>
2022-02-03 11:11:55 +0000

    learnt how to make use of embedded code into external files and calling it correctly.

--- Weapon.h
+++ Weapon.h
@@ -1,4 +1,4 @@
-#include <string>
+#include <iostream>
 
 class Weapon
 {
@@ -14,104 +14,34 @@ class Weapon
 
 	public:
 		//Constructor set to have default values for the weapon, unusable as it is, needs to be set on startup.
-			//Constructor Prototype Meth
 		Weapon();	
-		{
-			name= "Default Name";
-			material= "Default Material";
-			
-			minimum_damage= 0;
-			maximum_damage= 999;
-			range= 0.0f;
-			accuracy= 0.0f;
-			condition= false;
-		}
-		
-		//Getters and Setters;
-			//Adding in "function prototypes"
-		//Name
-
-		//Material
-
-		//Minimum Damage
-
-		//Maximum Damage
-
-		//Range
-
-		//Accuracy
-
-		//Condition
-		
-
+	
+		//Getter and Setter block
 		//Name:
-		std::string get_name()
-		{
-			return name;
-		}
+		std::string get_name();
+		void set_name(std::string new_name );
 		
-		void set_name(std::string new_name )
-		{
-			name= new_name;
-		}
 		//Material:
-		std::string get_material()
-		{
-			return material;
-		}
-
-		void set_material(std::string new_materials )
-		{
-			material= new_materials;
-		}
+		std::string get_material();
+		void set_material(std::string new_materials );
+		
 		//Minimum Damage
-		int get_minimum_damage()
-		{
-			return minimum_damage;
-		}
+		int get_minimum_damage();
+		void set_minimum_damage(int new_minimum_damage );
 		
-		void set_minimum_damage(int new_minimum_damage )
-		{
-			minimum_damage= new_minimum_damage;
-		}
 		//Maximum Damage
-		int get_maximum_damage()
-		{
-			return maximum_damage;
-		}
+		int get_maximum_damage();
+		void set_maximum_damage(int new_maximum_damage );
 		
-		void set_maximum_damage(int new_maximum_damage )
-		{
-			maximum_damage= new_maximum_damage;
-		}
 		//range
-		float get_range()
-		{
-			return range;
-		}
+		float get_range();
+		void set_range(float new_range );
 		
-		void set_range(float new_range )
-		{
-			range= new_range;
-		}
 		//accuracy
-		void set_accuracy(float new_accuracy )
-		{
-			accuracy= new_accuracy;
-		}
+		void set_accuracy(float new_accuracy );
+		float get_accuracy();
 		
-		float get_accuracy()
-		{
-			return accuracy;
-		}
 		//Condition
-		bool get_condition()
-		{
-			return condition;
-		}
-		
-		void set_condition(bool new_condition )
-		{
-			condition= new_condition;
-		}
+		bool get_condition();
+		void set_condition(bool new_condition );
 };
