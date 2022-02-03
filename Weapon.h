c5dc75b87d2d56486c1541a40381ab2135e7d1ad
kelvinfried <kelvinfriedchicken@gmail.com>
2022-02-03 09:58:35 +0000

    Moved all of the main's class code into the .h file as the first step.

--- Weapon.h
+++ Weapon.h
@@ -1,40 +1,117 @@
 #include <string>
 
-Weapon()
+class Weapon
 {
 	private:
-	//Attribute Definition
-		std::string Weapon::name;
-		std::string Weapon::material;
+		std::string name;
+		std::string material;
 		
-		int Weapon::minimum_damage;
-		int Weapon::maximum_damage;
-		float Weapon::range;
-		float Weapon::accuracy;
-		bool Weapon::condition;	//If it is in good or bad condition	
-		
+		int minimum_damage;
+		int maximum_damage;
+		float range;
+		float accuracy;
+		bool condition;	//If it is in good or bad condition	
+
 	public:
-	//Constructor 
-		Weapon::Weapon();
+		//Constructor set to have default values for the weapon, unusable as it is, needs to be set on startup.
+			//Constructor Prototype Meth
+		Weapon();	
+		{
+			name= "Default Name";
+			material= "Default Material";
+			
+			minimum_damage= 0;
+			maximum_damage= 999;
+			range= 0.0f;
+			accuracy= 0.0f;
+			condition= false;
+		}
+		
+		//Getters and Setters;
+			//Adding in "function prototypes"
 		//Name
-		std::string Weapon::get_name();
-		void Weapon::set_name(std::string new_name );
-		//Mateiral
-		std::string Weapon::get_material();
-		void Weapon::set_material(std::string new_materials );
-		//Min Damage
-		int Weapon::get_minimum_damage();
-		void Weapon::set_minimum_damage(int new_minimum_damage );
-		//Max Damage
-		int get_maximum_damage();
-		void Weapon::set_maximum_damage(int new_maximum_damage );
+
+		//Material
+
+		//Minimum Damage
+
+		//Maximum Damage
+
 		//Range
-		float Weapon::get_range();
-		void Weapon:: set_range(float new_range );
+
 		//Accuracy
-		float Weapon::get_accuracy();
-		void Weapon::set_accuracy(float new_accuracy );
-		//Conditon
-		bool Weapon::get_condition();
-		void Weapon::set_condition(bool new_condition );
+
+		//Condition
+		
+
+		//Name:
+		std::string get_name()
+		{
+			return name;
+		}
+		
+		void set_name(std::string new_name )
+		{
+			name= new_name;
+		}
+		//Material:
+		std::string get_material()
+		{
+			return material;
+		}
+
+		void set_material(std::string new_materials )
+		{
+			material= new_materials;
+		}
+		//Minimum Damage
+		int get_minimum_damage()
+		{
+			return minimum_damage;
+		}
+		
+		void set_minimum_damage(int new_minimum_damage )
+		{
+			minimum_damage= new_minimum_damage;
+		}
+		//Maximum Damage
+		int get_maximum_damage()
+		{
+			return maximum_damage;
+		}
+		
+		void set_maximum_damage(int new_maximum_damage )
+		{
+			maximum_damage= new_maximum_damage;
+		}
+		//range
+		float get_range()
+		{
+			return range;
+		}
+		
+		void set_range(float new_range )
+		{
+			range= new_range;
+		}
+		//accuracy
+		void set_accuracy(float new_accuracy )
+		{
+			accuracy= new_accuracy;
+		}
+		
+		float get_accuracy()
+		{
+			return accuracy;
+		}
+		//Condition
+		bool get_condition()
+		{
+			return condition;
+		}
+		
+		void set_condition(bool new_condition )
+		{
+			condition= new_condition;
+		}
 };
